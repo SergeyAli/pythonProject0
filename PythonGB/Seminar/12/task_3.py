e3f28da0255e644de81e02f484c0eba28d39632e
ASN <aliger99@rambler.ru>
2023-08-22 21:03:51 +0300

    Семинар_12_0

--- PythonGB/Seminar/12/task_3.py
+++ PythonGB/Seminar/12/task_3.py
@@ -6,4 +6,37 @@
 📌 Если передан один параметр, также считаем start=1.
 '''
 
+class GeneratorFactor:
 
+    def __init__(self, *args):
+
+        match len(args):
+            case 1:
+                self.start = 1
+                self.stop = args[0]
+                self.step = 1
+            case 2:
+
+                self.start, self.stop = args
+                self.step = 1
+            case 3:
+                self.start, self.stop, self.step = args
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+
+        while self.start < self.stop:
+            res = 1
+            for item in range(1, self.start + 1):
+                res *= item
+            self.start += self.step
+            return res
+        raise StopIteration
+
+
+if __name__ == "__main__":
+    factor = GeneratorFactor(5)
+    for item in factor:
+        print(item)
