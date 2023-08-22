4907a7747a5e60314dce51d390227c6893b4f7e3
ASN <aliger99@rambler.ru>
2023-08-22 21:23:47 +0300

    Семинар_12_0

--- PythonGB/Seminar/12/task_4.py
+++ PythonGB/Seminar/12/task_4.py
@@ -6,3 +6,50 @@
 '''
 
 
+class Rectangle:
+
+    def __init__(self, height: int, width=None):
+        self._height = height
+        if width:
+            self._width = width
+        else:
+            self._width = height
+
+    def get_perimetr(self):
+        return 2 * (self._height + self._width)
+
+    def get_area(self):
+        return self._width * self._height
+
+    @property
+    def height(self):
+        return self._height
+
+    @property
+    def width(self):
+        return self._width
+
+    @height.setter
+    def height(self, value):
+        if value <= 0:
+            raise ValueError("должно быть больше нуля")
+        self._height = value
+
+    @width.setter
+    def width(self, value):
+        if value <= 0:
+            raise ValueError("должно быть больше нуля")
+        self._width = value
+
+    def __str__(self):
+        return f'прямоугольник ({self._width}х{self._height}), S= {self.get_area()}'
+
+    def __repr__(self):
+        return f'размеры:({self._width}х{self._height}), S= {self.get_area()}'
+
+
+if __name__ == "__main__":
+    rect = Rectangle(2, 5)
+    rect.width = 0
+
+    print(rect)
